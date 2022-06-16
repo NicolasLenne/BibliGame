a98d514833255b7c6e550a564a628d13598e13c5
nicolaslenne <nicolas.lenne@outlook.fr>
2022-06-16 16:54:03 +0200

    accueil pratiquement fini, commencement de la page de login

--- Symfony/src/Controller/MainController.php
+++ Symfony/src/Controller/MainController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
@@ -16,5 +17,31 @@ class MainController extends AbstractController
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
+    }
+
+        /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+      {
+         // get the login error if there is one
+         $error = $authenticationUtils->getLastAuthenticationError();
+
+         // last username entered by the user
+         $lastUsername = $authenticationUtils->getLastUsername();
+
+          return $this->render('main/login.html.twig', [
+             'last_username' => $lastUsername,
+             'error'         => $error,
+          ]);
+      }
+
+        /**
+     * @Route("/logout", name="logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        // // controller can be blank: it will never be called!
+        // return $this->redirectToRoute('login', [], Response::HTTP_SEE_OTHER);
     }
 }
