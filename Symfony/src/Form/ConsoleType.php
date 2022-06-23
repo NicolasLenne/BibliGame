01af2c12295f4fcc34049c776aa559239e885d9f
NicolasLenne <nicolas.lenne@outlook.fr>
2022-06-23 05:41:38 +0200

    page d'ajout d'une console en cours

--- Symfony/src/Form/ConsoleType.php
+++ Symfony/src/Form/ConsoleType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Console;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,21 @@ class ConsoleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('isLoose')
-            ->add('photo')
-            ->add('comment')
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' =>  ['class' => 'input'],
+            ])
+            ->add('isLoose', CheckboxType::class, [
+                'label' => false,
+            ])
+            ->add('photo', TextType::class, [
+                'label' => false,
+                'attr' =>  ['class' => 'input'],
+            ])
+            ->add('comment', TextareaType::class, [
+                'label' => false,
+                'attr' =>  ['class' => 'textarea'],
+            ])
         ;
     }
 
