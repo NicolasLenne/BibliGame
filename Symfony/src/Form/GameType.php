c975c226b1fe6b46900251b5210820ae3fd81f47
nicolaslenne <nicolas.lenne@outlook.fr>
2022-06-24 17:40:18 +0200

    modification du gametype pour n'afficher que les consoles lié à l'utilisateur

--- Symfony/src/Form/GameType.php
+++ Symfony/src/Form/GameType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Game;
 use App\Entity\Console;
+use App\Repository\ConsoleRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -11,9 +12,19 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class GameType extends AbstractType
 {
+    protected $tokenStorage;
+    private $consoles;
+
+    public function __construct(TokenStorageInterface $tokenStorage, ConsoleRepository $consoleRepository)
+    {
+            $this->tokenStorage = $tokenStorage->getToken()->getUser();
+            $this->consoles = $consoleRepository;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -36,6 +47,7 @@ class GameType extends AbstractType
                 'label' => false,
                 'class' => Console::class,
                 'choice_label' => 'name',
+                'choices' => $this->consoles->findBy(['user' => $this->tokenStorage]),
                 'multiple' => false,
                 'placeholder' => 'Selectionnez la console',
                 'expanded' => false
