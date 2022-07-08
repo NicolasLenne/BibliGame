df619f7469de85a3ac23d58c0b44980e11d84b92
nicolaslenne <nicolas.lenne@outlook.fr>
2022-07-08 17:49:41 +0200

    sylisation d'upload image et mise en place des contrainte sur l'ajout de jeux

--- Symfony/src/Form/GameType.php
+++ Symfony/src/Form/GameType.php
@@ -8,7 +8,9 @@ use App\Repository\ConsoleRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -35,9 +37,20 @@ class GameType extends AbstractType
             ->add('isLoose', CheckboxType::class, [
                 'label' => false,
             ])
-            ->add('photo', TextType::class, [
+            ->add('photo', FileType::class, [
+                'attr' => ['class' => 'file-input'],
                 'label' => false,
-                'attr' =>  ['class' => 'input'],
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Seul les formats .jpg',
+                    ])
+                ],
             ])
             ->add('comment', TextareaType::class, [
                 'label' => false,
